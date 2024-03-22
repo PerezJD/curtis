d8b23e5847c6596af619e707342d8fa6579916c3
James Perez <jdperez04@gmail.com>
2024-03-22 17:00:26 -0400

    remove console logs and clean up

--- src/client/components/markdown/MultilineMarkdownComponent.tsx
+++ src/client/components/markdown/MultilineMarkdownComponent.tsx
@@ -5,15 +5,6 @@ import {compiler} from "markdown-to-jsx";
 import markdownOptions from "./markdownOptions";
 
 const MultilineMarkdownComponent: FunctionComponent<MultilineMarkdownComponentProps> = ({ lines }) => {
-
-  const renderedContent = lines.reduce((acc, line) => {
-    const renderedLine = compiler(line, markdownOptions);
-    console.log(renderedLine);
-    return `${acc}${renderedLine}\n\n`
-  }, '');
-
-  console.log(renderedContent);
-
   return (
     <>
       { lines.map((line) => compiler(`${line}\n\n`, markdownOptions)) }
